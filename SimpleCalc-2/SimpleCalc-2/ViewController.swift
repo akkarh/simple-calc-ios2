9701fc2262748bbedea5efc707b95c7372640851
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-02-01 15:11:32 -0800

    Created XCode project

---
+++ SimpleCalc-2/SimpleCalc-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalc-2
+//
+//  Created by Harshitha Akkaraju on 2/1/18.
+//  Copyright © 2018 Harshitha Akkaraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
